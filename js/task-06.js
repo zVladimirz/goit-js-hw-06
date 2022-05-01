bb09b2626aead06743fe231bcbe93e2e683016c2
zVladimirz <tzxdlo@gmail.com>
2022-05-01 09:05:21 +0300

    1

--- js/task-06.js
+++ js/task-06.js
@@ -3,6 +3,7 @@ inputEl.addEventListener('blur', () => {
   const inputText = inputEl.value;
   const inputTextLen = inputEl.dataset.length;
 console.log(inputTextLen);
+console.log(inputText.length);
   if (inputText.length === inputTextLen) {
     inputEl.classList.add('valid');
     inputEl.classList.remove('invalid');
