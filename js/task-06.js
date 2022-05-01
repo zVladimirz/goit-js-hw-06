49383e283fc3c02811e9af3cb6da7fc7b3f60e6b
zVladimirz <tzxdlo@gmail.com>
2022-05-01 09:15:41 +0300

    6

--- js/task-06.js
+++ js/task-06.js
@@ -2,8 +2,6 @@ const inputEl = document.querySelector('#validation-input');
 inputEl.addEventListener('blur', () => {
   const inputText = inputEl.value;
   const inputTextLen = Number(inputEl.dataset.length);
-console.log(inputTextLen);
-console.log(inputText.length);
   if (inputText.length === inputTextLen) {
     inputEl.classList.add('valid');
     inputEl.classList.remove('invalid');
